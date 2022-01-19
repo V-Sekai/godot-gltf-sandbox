9f9317559eb59fe1c9d31982b4999238ed2a7056
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-01-19 04:08:45 -0800

    Update sample project to the latest Godot Engine.

--- addons/mozilla_hubs/import_mozilla_hubs.gd
+++ addons/mozilla_hubs/import_mozilla_hubs.gd
@@ -1,5 +1,5 @@
 @tool
-extends EditorSceneImporter
+extends EditorSceneFormatImporter
 
 # Set this to true to save a .res file with all GLTF DOM state
 # This allows exploring all JSON structure and also Godot internal GLTFState
@@ -8,26 +8,29 @@ const SAVE_DEBUG_GLTFSTATE_RES: bool = false
 
 
 func _get_extensions():
-	return ["mozhubs"]
+	return ["gltf", "glb"]
 
 
 func _get_import_flags():
-	return EditorSceneImporter.IMPORT_SCENE
+	return EditorSceneFormatImporter.IMPORT_SCENE
 
 
 func _import_animation(path: String, flags: int, bake_fps: int) -> Animation:
 	return Animation.new()
 
 
-func _import_scene(path: String, flags: int, bake_fps: int):
+func _import_scene(path: String, flags: int, options: Dictionary, bake_fps: int):
 	var f = File.new()
 	var new_glb = path.get_basename() + ".glb"
 	if f.open(new_glb, File.READ) != OK:
 		return FAILED
 		
 	var gstate : GLTFState = GLTFState.new()
-	var gltf : PackedSceneGLTF = PackedSceneGLTF.new()
-	var root_node : Node = gltf.import_gltf_scene(new_glb, 0, 1000.0, gstate)
+	var gltf : GLTFDocument = GLTFDocument.new()
+	var err = gltf.append_from_file(new_glb, gstate)
+	if err != OK:
+		return null
+	var root_node : Node = gltf.generate_scene(gstate)
 	
 	if not gstate.json.has(StringName("nodes")):
 		return ERR_PARSE_ERROR
@@ -64,7 +67,6 @@ func _import_scene(path: String, flags: int, bake_fps: int):
 			
 		if hubs.is_empty():
 			continue
-		
 		var keys : Array = hubs.keys()	
 					
 		if keys.has("visible"):
@@ -144,11 +146,3 @@ func _import_scene(path: String, flags: int, bake_fps: int):
 	var packed_scene: PackedScene = PackedScene.new()
 	packed_scene.pack(root_node)
 	return packed_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
-
-
-func import_animation_from_other_importer(path: String, flags: int, bake_fps: int):
-	return self._import_animation(path, flags, bake_fps)
-
-
-func import_scene_from_other_importer(path: String, flags: int, bake_fps: int):
-	return self._import_scene(path, flags, bake_fps)
--- addons/mozilla_hubs/plugin.gd
+++ addons/mozilla_hubs/plugin.gd
@@ -6,9 +6,9 @@ var import_plugin
 
 func _enter_tree():
 	import_plugin = preload("res://addons/mozilla_hubs/import_mozilla_hubs.gd").new()
-	add_scene_import_plugin(import_plugin)
+	add_scene_format_importer_plugin(import_plugin)
 
 
 func _exit_tree():
-	remove_scene_import_plugin(import_plugin)
+	remove_scene_format_importer_plugin(import_plugin)
 	import_plugin = null
